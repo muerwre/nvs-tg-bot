9cfd6f5e6a9bea7514be782dbdaf61cbd4eee048
muerwre <gotham48@gmail.com>
2019-03-18 11:13:04 +0700

    added post callback

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import get from './main/get';
+import post from './main/post';
 
 const router = Router();
 router.get('/', get);
+router.post('/', post);
 
 export default router;

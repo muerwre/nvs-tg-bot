6e0e1695b80735c94cefa7be6c9766d2b21f6ec0
muerwre <gotham48@gmail.com>
2019-03-20 11:49:52 +0700

    removed previews on group join messages

--- src/responsers/groupJoinResponder.ts
+++ src/responsers/groupJoinResponder.ts
@@ -13,7 +13,8 @@ export const groupJoinResponder = async (req: express.Request, res: express.Resp
 
   const { type, object: { user_id } } = req.body as IUserObject;
   const text = `https://vk.com/id${user_id} ${type === 'group_join' ? 'присоединился к группе' : 'Свалил из группы'}`;
-  await bot.telegram.sendMessage(chat, text).catch(() => null);
+  const extras = { parse_mode: 'Markdown', disable_web_page_preview: true };
+  await bot.telegram.sendMessage(chat, text, extras).catch(() => null);
 
   return true;
 };

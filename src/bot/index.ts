700348fd0dccdd1ca09d6126338960e1926a6f8b
Fedor Katurov <gotham48@gmail.com>
2020-01-28 09:59:37 +0700

    throttling messages

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -3,40 +3,48 @@ import { makeKB } from "../utils/merkup";
 import { EMOTIONS } from "../const";
 import { Vote } from "../models/Vote";
 import { Post } from "../models/Post";
+import { throttle } from "throttle-debounce";
 
-const SocksProxyAgent = require('socks-proxy-agent');
-const Telegraf = require('telegraf');
+const SocksProxyAgent = require("socks-proxy-agent");
+const Telegraf = require("telegraf");
 
 const agent = (CONFIG.PROXY && new SocksProxyAgent(CONFIG.PROXY)) || null;
 const options = {
   channelMode: true,
   telegram: { agent },
-  agent,
+  agent
 };
 
 const bot = new Telegraf(CONFIG.TELEGRAM.key, options);
 
-bot.command('ping', async (ctx, next) => {
-  return await ctx.reply(`pong`)
+bot.command("ping", async (ctx, next) => {
+  return await ctx.reply(`pong`);
 });
 
 if (CONFIG.REACTIONS && CONFIG.REACTIONS.length) {
   CONFIG.REACTIONS.map(({ match, from, reply }) => {
     if (!reply || !match) return;
-    
-    bot.hears(match, async (ctx, next) => {    
-      if (!!from && from !== ctx.message.from.username) {
+
+    bot.hears(
+      match,
+      throttle(60000, async (ctx, next) => {
+        if (
+          ctx.message.from.is_bot ||
+          (!!from && from !== ctx.message.from.username)
+        ) {
+          return next();
+        }
+
+        await ctx.reply(reply, { reply_to_message_id: ctx.message.message_id });
         return next();
-      }
-      
-      await ctx.reply(reply, { 'reply_to_message_id': ctx.message.message_id })
-      return next();
-    });
+      })
+    );
   });
 }
 
-bot.action(/emo \[(\d+)\]/, async (ctx) => {
-  const { message = {}, from = {} } = ctx && ctx.update && ctx.update.callback_query || { };
+bot.action(/emo \[(\d+)\]/, async ctx => {
+  const { message = {}, from = {} } =
+    (ctx && ctx.update && ctx.update.callback_query) || {};
   const { match = [] } = ctx;
   const emo_id = (match[1] && parseInt(match[1])) || 0;
 
@@ -55,20 +63,37 @@ bot.action(/emo \[(\d+)\]/, async (ctx) => {
   }
 
   // count all likes from db by type
-  const emos = await Vote.find({ message_id, chat_id }).then(result => (
-    result.reduce((obj, emo) => ({
-      ...obj,
-      [emo.emo_id]: (obj[emo.emo_id] || 0) + 1,
-    }), {})
-  ));
+  const emos = await Vote.find({ message_id, chat_id }).then(result =>
+    result.reduce(
+      (obj, emo) => ({
+        ...obj,
+        [emo.emo_id]: (obj[emo.emo_id] || 0) + 1
+      }),
+      {}
+    )
+  );
 
   // create like array filled with likes from db
-  const list = Object.keys(EMOTIONS).map((em, i) => (emos[i] || 0));
+  const list = Object.keys(EMOTIONS).map((em, i) => emos[i] || 0);
 
   const post = await Post.findOne({ chat_id, message_id });
-  const { map_url = null, post_url = null, is_cutted = false, topic_url = null, album_url = null } = (post || {});
+  const {
+    map_url = null,
+    post_url = null,
+    is_cutted = false,
+    topic_url = null,
+    album_url = null
+  } = post || {};
 
-  const extras = { inline_keyboard: makeKB(list, { map_url, post_url, is_cutted, topic_url, album_url }) };
+  const extras = {
+    inline_keyboard: makeKB(list, {
+      map_url,
+      post_url,
+      is_cutted,
+      topic_url,
+      album_url
+    })
+  };
 
   await ctx.editMessageReplyMarkup(extras).catch(console.log);
 });

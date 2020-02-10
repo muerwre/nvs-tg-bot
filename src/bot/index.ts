c6d9a83717572a9886ad27a6962492ed41da893c
Fedor Katurov <gotham48@gmail.com>
2020-02-10 15:57:58 +0700

    added catch to axios

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -4,7 +4,7 @@ import { makeKB } from "../utils/merkup";
 import { EMOTIONS } from "../const";
 import { Vote } from "../entity/Vote";
 import { Post } from "../entity/Post";
-import axios from 'axios';
+import axios from "axios";
 
 const SocksProxyAgent = require("socks-proxy-agent");
 const Telegraf = require("telegraf");
@@ -22,21 +22,25 @@ bot.command("ping", async (ctx, next) => {
   return await ctx.reply(`pong`);
 });
 
-bot.hears(/^\/roll\s?(\d{0,})\s?(\d{0,})?/igm, async (ctx, next) => {
-  const min = Math.min(ctx.match[1] || 0, ctx.match[2] || 0)
-  const max = Math.max(ctx.match[1] || 0, ctx.match[2] || 0)
+bot.hears(/^\/roll\s?(\d{0,})\s?(\d{0,})?/gim, async (ctx, next) => {
+  const min = Math.min(ctx.match[1] || 0, ctx.match[2] || 0);
+  const max = Math.max(ctx.match[1] || 0, ctx.match[2] || 0);
 
-  const reply = await axios.get(CONFIG.FEATURES.RANDOM_URL.PROVIDER, { params: { min, max } })
+  const reply = await axios
+    .get(CONFIG.FEATURES.RANDOM_URL.PROVIDER, { params: { min, max } })
+    .catch(() => {});
 
   if (!reply || !reply.data || !reply.data.id) {
     return next();
   }
 
-  const description = reply.data.description ? `${reply.data.description}\n` : '';
+  const description = reply.data.description
+    ? `${reply.data.description}\n`
+    : "";
   return await ctx.reply(
     `${reply.data.title} (${reply.data.distance}км):\n${description}\n${CONFIG.FEATURES.RANDOM_URL.HOST}${reply.data.id}`,
     {
-      disable_web_page_preview: true,
+      disable_web_page_preview: true
     }
   );
 });
@@ -60,25 +64,33 @@ bot.action(/emo \[(\d+)\]/, async ctx => {
     vote.emo_id = emo_id;
     await vote.save();
   } else {
-    await Vote.create({ user_id, message_id, emo_id, chat_id: chat_id.toString() }).save();
+    await Vote.create({
+      user_id,
+      message_id,
+      emo_id,
+      chat_id: chat_id.toString()
+    }).save();
   }
 
   // count all likes from db by type
-  const emos = await Vote.find({ where: { message_id, chat_id: chat_id.toString() } }).then(
-    result =>
-      result.reduce(
-        (obj, emo) => ({
-          ...obj,
-          [emo.emo_id]: (obj[emo.emo_id] || 0) + 1
-        }),
-        {}
-      )
+  const emos = await Vote.find({
+    where: { message_id, chat_id: chat_id.toString() }
+  }).then(result =>
+    result.reduce(
+      (obj, emo) => ({
+        ...obj,
+        [emo.emo_id]: (obj[emo.emo_id] || 0) + 1
+      }),
+      {}
+    )
   );
 
   // create like array filled with likes from db
   const list = Object.keys(EMOTIONS).map((em, i) => emos[i] || 0);
 
-  const post = await Post.findOne({ where: { chat_id: chat_id.toString(), message_id }});
+  const post = await Post.findOne({
+    where: { chat_id: chat_id.toString(), message_id }
+  });
   const {
     map_url = null,
     post_url = null,

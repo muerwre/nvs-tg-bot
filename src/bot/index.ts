3b854d5a816b86f0ab6677d8615bafe4438aff09
Fedor Katurov <gotham48@gmail.com>
2020-03-18 11:11:39 +0700

    added error catchers

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -5,7 +5,7 @@ import { EMOTIONS } from "../const";
 import { Vote } from "../entity/Vote";
 import { Post } from "../entity/Post";
 // import { rollResponser } from "../responsers/rollResponser";
-import Axios from "axios";
+// import Axios from "axios";
 
 const SocksProxyAgent = require("socks-proxy-agent");
 const Telegraf = require("telegraf");
@@ -19,32 +19,32 @@ const options = {
 
 const bot = new Telegraf(CONFIG.TELEGRAM.key, options);
 
-bot.on("inline_query", async ({ inlineQuery, answerInlineQuery }) => {
-  const url = `${CONFIG.FEATURES.SEARCH.URL}${encodeURIComponent(inlineQuery.query)}`;
-  const response = await Axios.get(url);
+// bot.on("inline_query", async ({ inlineQuery, answerInlineQuery }) => {
+//   const url = `${CONFIG.FEATURES.SEARCH.URL}${encodeURIComponent(inlineQuery.query)}`;
+//   const response = await Axios.get(url);
 
-  console.log({ url })
-  console.log({ data: response.data.routes })
+//   console.log({ url })
+//   console.log({ data: response.data.routes })
 
-  const routes =
-    response && response.data && response.data.routes
-      ? response.data.routes
-          .filter(({ address }) => address)
-          .map(({ address, title, distance, description }) => ({
-            type: "article",
-            id: address,
-            title,
-            description: `${title} (${distance}км)\n${CONFIG.FEATURES.SEARCH.HOST}${address}`,
-            input_message_content: {
-              message_text: `${title} (${distance}км)\n${CONFIG.FEATURES.SEARCH.HOST}${address}`
-            }
-          }))
-      : [];
+//   const routes =
+//     response && response.data && response.data.routes
+//       ? response.data.routes
+//           .filter(({ address }) => address)
+//           .map(({ address, title, distance, description }) => ({
+//             type: "article",
+//             id: address,
+//             title,
+//             description: `${title} (${distance}км)\n${CONFIG.FEATURES.SEARCH.HOST}${address}`,
+//             input_message_content: {
+//               message_text: `${title} (${distance}км)\n${CONFIG.FEATURES.SEARCH.HOST}${address}`
+//             }
+//           }))
+//       : [];
 
-  console.log(routes);
-  return answerInlineQuery(routes, { disable_web_page_preview: true }); 
-  // return answerInlineQuery([]); 
-});
+//   console.log(routes);
+//   return answerInlineQuery(routes, { disable_web_page_preview: true });
+//   // return answerInlineQuery([]);
+// });
 
 bot.command("ping", async (ctx, next) => {
   return await ctx.reply(`pong`);
@@ -53,70 +53,76 @@ bot.command("ping", async (ctx, next) => {
 // bot.hears(/^\/roll\s?(\d{0,})\s?(\d{0,})?/gim, rollResponser);
 
 bot.action(/emo \[(\d+)\]/, async ctx => {
-  const { message = {}, from = {} } =
-    (ctx && ctx.update && ctx.update.callback_query) || {};
+  try {
+    const { message = {}, from = {} } =
+      (ctx && ctx.update && ctx.update.callback_query) || {};
 
-  const { match = [] } = ctx;
-  const emo_id = (match[1] && parseInt(match[1])) || 0;
+    const { match = [] } = ctx;
+    const emo_id = (match[1] && parseInt(match[1])) || 0;
 
-  if (!message.message_id || !from.id) return;
+    if (!message.message_id || !from.id) return;
 
-  const user_id: number = from.id;
-  const message_id: number = message.message_id;
-  const chat_id: number = message.chat.id;
+    const user_id: number = from.id;
+    const message_id: number = message.message_id;
+    const chat_id: number = message.chat.id;
 
-  const vote = await Vote.findOne({ where: { user_id, message_id, chat_id } });
+    const vote = await Vote.findOne({
+      where: { user_id, message_id, chat_id }
+    });
 
-  if (vote) {
-    vote.emo_id = emo_id;
-    await vote.save();
-  } else {
-    await Vote.create({
-      user_id,
-      message_id,
-      emo_id,
-      chat_id: chat_id.toString()
-    }).save();
+    if (vote) {
+      vote.emo_id = emo_id;
+      await vote.save();
+    } else {
+      await Vote.create({
+        user_id,
+        message_id,
+        emo_id,
+        chat_id: chat_id.toString()
+      }).save();
+    }
+
+    // count all likes from db by type
+    const emos = await Vote.find({
+      where: { message_id, chat_id: chat_id.toString() }
+    }).then(result =>
+      result.reduce(
+        (obj, emo) => ({
+          ...obj,
+          [emo.emo_id]: (obj[emo.emo_id] || 0) + 1
+        }),
+        {}
+      )
+    );
+
+    // create like array filled with likes from db
+    const list = Object.keys(EMOTIONS).map((em, i) => emos[i] || 0);
+
+    const post = await Post.findOne({
+      where: { chat_id: chat_id.toString(), message_id }
+    });
+    const {
+      map_url = null,
+      post_url = null,
+      is_cutted = false,
+      topic_url = null,
+      album_url = null
+    } = post || {};
+
+    const extras = {
+      inline_keyboard: makeKB(list, {
+        map_url,
+        post_url,
+        is_cutted,
+        topic_url,
+        album_url
+      })
+    };
+
+    await ctx.editMessageReplyMarkup(extras).catch(console.log);
+  } catch (e) {
+    console.log(e);
   }
-
-  // count all likes from db by type
-  const emos = await Vote.find({
-    where: { message_id, chat_id: chat_id.toString() }
-  }).then(result =>
-    result.reduce(
-      (obj, emo) => ({
-        ...obj,
-        [emo.emo_id]: (obj[emo.emo_id] || 0) + 1
-      }),
-      {}
-    )
-  );
-
-  // create like array filled with likes from db
-  const list = Object.keys(EMOTIONS).map((em, i) => emos[i] || 0);
-
-  const post = await Post.findOne({
-    where: { chat_id: chat_id.toString(), message_id }
-  });
-  const {
-    map_url = null,
-    post_url = null,
-    is_cutted = false,
-    topic_url = null,
-    album_url = null
-  } = post || {};
-
-  const extras = {
-    inline_keyboard: makeKB(list, {
-      map_url,
-      post_url,
-      is_cutted,
-      topic_url,
-      album_url
-    })
-  };
-
-  await ctx.editMessageReplyMarkup(extras).catch(console.log);
 });
 
 bot.launch();
--- src/routes/main/post.ts
+++ src/routes/main/post.ts
@@ -1,41 +1,58 @@
-import { CHANNELS, CONFIG, TYPES } from '$config/server';
+import { CHANNELS, CONFIG, TYPES } from "$config/server";
 import { OK_RESPONSE, RESPONSERS } from "../../const";
 
-import * as express from 'express';
+import * as express from "express";
 
-const handler = async (req: express.Request, res: express.Response): Promise<express.Response> => {
-  const { body: { type, group_id, secret, object } } = req;
+const handler = async (
+  req: express.Request,
+  res: express.Response
+): Promise<express.Response> => {
+  try {
+    const {
+      body: { type, group_id, secret, object }
+    } = req;
 
-  if (group_id !== CONFIG.VK.group_id || (type !== TYPES.CONFIRMATION && secret !== CONFIG.VK.secret_key)) {
-    return res.status(401).send('Invalid credentials');
-  }
-
-  if (type === TYPES.CONFIRMATION) {
-    return RESPONSERS[type](req, res);
-  } else if (type === TYPES.NEW_POST && object && object.post_type === 'suggest') {
-    // intercept post suggestions
-    if (CHANNELS[TYPES.POST_SUGGESTION] && RESPONSERS[TYPES.POST_SUGGESTION]) {
-      await Promise.all(
-        CHANNELS[TYPES.POST_SUGGESTION].map(chan => (
-          RESPONSERS[TYPES.POST_SUGGESTION](req, res, chan)
-        ))
-      );
+    if (
+      group_id !== CONFIG.VK.group_id ||
+      (type !== TYPES.CONFIRMATION && secret !== CONFIG.VK.secret_key)
+    ) {
+      return res.status(401).send("Invalid credentials");
     }
-  } else {
-    if (CHANNELS[type]) {
-      await Promise.all(
-        CHANNELS[type].map(chan => (
-          (RESPONSERS[type] && RESPONSERS[type](req, res, chan)) || RESPONSERS.DEFAULT(req, res)
-        ))
-      );
+
+    if (type === TYPES.CONFIRMATION) {
+      return RESPONSERS[type](req, res);
+    } else if (
+      type === TYPES.NEW_POST &&
+      object &&
+      object.post_type === "suggest"
+    ) {
+      // intercept post suggestions
+      if (
+        CHANNELS[TYPES.POST_SUGGESTION] &&
+        RESPONSERS[TYPES.POST_SUGGESTION]
+      ) {
+        await Promise.all(
+          CHANNELS[TYPES.POST_SUGGESTION].map(chan =>
+            RESPONSERS[TYPES.POST_SUGGESTION](req, res, chan)
+          )
+        );
+      }
+    } else {
+      if (CHANNELS[type]) {
+        await Promise.all(
+          CHANNELS[type].map(
+            chan =>
+              (RESPONSERS[type] && RESPONSERS[type](req, res, chan)) ||
+              RESPONSERS.DEFAULT(req, res)
+          )
+        );
+      }
     }
+
+    return res.send(OK_RESPONSE);
+  } catch (e) {
+    console.log(e);
   }
-
-  // return (RESPONSERS[type] && RESPONSERS[type](req, res)) || RESPONSERS.DEFAULT(req, res);
-
-
-
-  return res.send(OK_RESPONSE);
 };
 
 export default handler;

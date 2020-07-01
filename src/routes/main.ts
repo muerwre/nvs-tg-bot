55c968b816b76818e0d60975a84457fe156f92fb
Fedor Katurov <gotham48@gmail.com>
2020-07-01 10:50:11 +0700

    added healthcheck route

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,12 +1,14 @@
-import { Router } from 'express';
-import get from './main/get';
-import post from './main/post';
-import ping from './main/ping';
+import { Router } from "express";
+import get from "./main/get";
+import post from "./main/post";
+import ping from "./main/ping";
+import healthcheck from "./main/healthcheck";
 
 const router = Router();
 
-router.get('/', get);
-router.post('/', post);
-router.post('/ping', ping);
+router.get("/", get);
+router.post("/", post);
+router.post("/ping", ping);
+router.get("/healthcheck", healthcheck);
 
 export default router;
---
+++ src/routes/main/healthcheck.ts
@@ -0,0 +1,7 @@
+import bot from "src/bot";
+
+export default (req, res) =>
+  bot.telegram
+    .getMe()
+    .then(() => res.sendStatus(200))
+    .catch(() => res.sendStatus(503));

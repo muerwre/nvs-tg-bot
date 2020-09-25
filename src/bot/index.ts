97c740c4288ea4f8de636eb31b52c0a0ce21a073
Fedor Katurov <gotham48@gmail.com>
2020-09-25 13:08:38 +0700

    removed old webhook binding

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -17,12 +17,6 @@ const options = {
 
 const bot = new Telegraf(CONFIG.TELEGRAM.key, options);
 
-if (CONFIG.HTTP.WEBHOOK_HOST && CONFIG.HTTP.WEBHOOK_URL) {
-  const url = new URL(CONFIG.HTTP.WEBHOOK_URL, CONFIG.HTTP.WEBHOOK_HOST);
-  console.log(`Bot webhook started at ${url.href}`);
-  bot.telegram.setWebhook(url.href).then(console.log, console.log);
-}
-
 bot.command('ping', async (ctx) => {
   checkHealth().then(
     () =>
